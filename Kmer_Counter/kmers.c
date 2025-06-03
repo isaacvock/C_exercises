98093727d211f94353412f2d1677db743a4fa2a4
isaacvock <isaac.vock@gmail.com>
2025-06-02 20:23:41 -0400

    Polished simpler kmer counter

--- Kmer_Counter/kmers.c
+++ Kmer_Counter/kmers.c
@@ -19,14 +19,60 @@ size_t count_kmers(const char *seq, char *query);
 
 int main(int argc, char *argv[]){
 
+
+    /* Make sure correct number of arguments */
+    if (argc != 3){
+        fprintf(stderr,
+        "Usage: %s <read sequence> <k> \n"
+        " <read sequence> : a sequence of As, Gs, Ts, and Cs\n"
+        " <k> : k-mer length to search for\n",
+        argv[0]
+        );
+        return EXIT_FAILURE;
+    }
+
     /* Parse input sequence */
 
     const char *seq = argv[1];
+    const char *seqcheck = argv[1];
+
+    /* Check validity of input sequence */
+
+    while(*seqcheck != '\0'){
+
+        for(size_t i = 0; i < 4; i++){
+
+            if(ALPHABET[i] == *seqcheck){
+                seqcheck++;
+                break;
+            }else if(i == 3){
+
+                fprintf(stderr,
+                "Error: <read sequence> must be a sequence of As, Gs, Ts, and Cs\n");
+                return EXIT_FAILURE;
+
+            }
+
+        }
+
+    }
     
     /* Parse k */
 
     char *endptr = NULL;
     unsigned long long tmp = strtoull(argv[2], &endptr, 10);
+
+    errno = 0;
+
+    if (errno == ERANGE ||           /* out of range for unsigned long long */
+        *endptr != '\0' ||           /* trailing junk like "123abc"         */
+        tmp == 0) {                  /* 0 is not a sensible k     */
+        fprintf(stderr,
+                "Error: <k> must be a positive integer between 1 and %zu\n",
+                SIZE_MAX);
+        return EXIT_FAILURE;
+    }
+
     size_t k = (size_t)tmp;
 
     /* Count all kmers */
@@ -46,6 +92,7 @@ int main(int argc, char *argv[]){
         size_t tmp = i;
         for(int pos = k -1; pos >=0; --pos){
 
+            /* Cutsey trick to output in lexicographical order */
             s[pos] = ALPHABET[tmp & 3];
             tmp >>= 2;
 
@@ -92,3 +139,4 @@ size_t count_kmers(const char *seq, char *query){
     return count;
     
 }
+
